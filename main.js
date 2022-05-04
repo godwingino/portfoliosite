816a85c5efed622b3d6d5205fff02b114ca37e0d
godwingino <104904093+godwingino@users.noreply.github.com>
2022-05-04 17:16:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+let menu=document.querySelector('#menu-icon');
+let navbar=document.querySelector('#navbar');
+let header=document.querySelector('header');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');  
+}
+window.onscroll = () => {
+     menu.classList.remove('bx-x');
+    navbar.classList.remove('active');  
+}
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+    
+});
+
+const sr = ScrollReveal ({
+    distance: '60px';
+    duration: 2500,
+    delay:400,
+    reset: true
+})
+
+sr.reveal('.home-text',{delay:200, origin:'top'})
